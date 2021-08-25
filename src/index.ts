ae4a8d40a64e7f430b7077f1d078e4242d398acf
Rachit-gaur <rayy.gaur@outlook.com>
2021-08-25 19:10:32 +0530

    merging changes

--- src/index.ts
+++ src/index.ts
@@ -124,55 +124,55 @@ const startup = async () => {
             console.log(JSON.stringify(await generateWallet(command[1], flags.testnet), null, 2));
             break;
         case 'export':
-            exportWallets(process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] : question('Enter password to access wallet store:', {
+            process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :exportWallets(process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] : question('Enter password to access wallet store:', {
                 hideEchoBack: true,
-            }), flags.path);
+            }));
             break;
         case 'generatemanagedwallet':
             await storeWallet(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
                 }), flags.path);
             break;
         case 'storemanagedwallet':
             await storeWallet(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
-                }), flags.path, question('Enter mnemonic to store:', {
+                }), question('Enter mnemonic to store:', {
                     hideEchoBack: true,
                 }));
             break;
         case 'storemanagedprivatekey':
             await storePrivateKey(command[1], flags.testnet,
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
                 }), question('Enter private key to store:', {
                     hideEchoBack: true,
-                }), flags.path);
+                }));
             break;
         case 'getmanagedwallet':
             await getWallet(command[1],
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] : question('Enter password to access wallet store:', {
                     hideEchoBack: true,
                 }), flags.path);
             break;
         case 'getprivatekey':
             await getPrivateKey(command[1], command[2],
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
-                }), flags.path);
+                }));
             break;
         case 'getaddress':
             await getAddress(command[1], command[2],
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
-                }), flags.path);
+                }));
             break;
         case 'removewallet':
             await removeWallet(command[1],
-                question('Enter password to access wallet store:', {
+                process.env["STOREPASSWORD"] ? process.env["STOREPASSWORD"] :question('Enter password to access wallet store:', {
                     hideEchoBack: true,
-                }), flags.path);
+                }));
             break;
         default:
             console.error('Unsupported command. Use tatum-kms --help for details.');
