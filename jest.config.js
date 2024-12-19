5d674dbad3846c45a6e48b0dda6d047f0110201c
martintatum <martin.kyselak@tatum.io>
2024-12-19 16:19:20 +0100

    ALL-9118 - Upgrade Node, NPM and other libraries
    Adding skeleton for Jest tests + first parametrized tests as an example

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+    testMatch: ['**/tests/**/*.test.ts'],
+  };
